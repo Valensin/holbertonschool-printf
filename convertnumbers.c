ef1fc903fc5e2fe08193ae60e212c82f79e35505
Valensin <Onilr31@gmail.com>
2022-11-13 07:15:09 -0400

    converts numbers (UPDATE)

--- convertnumbers.c
+++ convertnumbers.c
@@ -1,5 +1,5 @@
 #include "main.h"
-ll
+
 unsigned int convert_di(va_list args, buffer_t *output,
 		unsigned char flags, int wid, int prec, unsigned char len);
 unsigned int convert_b(va_list args, buffer_t *output,
